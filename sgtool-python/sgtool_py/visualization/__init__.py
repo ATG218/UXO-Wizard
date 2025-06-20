f736f9906548eb915f61a34a66436cff5a5f03f1
ATG218 <alextgarbuz@gmail.com>
2025-06-20 16:12:03 +0200

    fix structure

--- sgtool-python/sgtool_py/visualization/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Visualization modules for interactive maps and plots."""
-
-from .interactive_maps import InteractiveMaps
-from .colormaps import GeophysicalColormaps
-
-__all__ = ["InteractiveMaps", "GeophysicalColormaps"]
