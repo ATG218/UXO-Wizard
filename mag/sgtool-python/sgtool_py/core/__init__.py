aba35286b2ddd5786f09e9dc0faf80b5e17a18d7
ATG218 <alextgarbuz@gmail.com>
2025-06-23 12:44:47 +0200

    remove mag from main

--- mag/sgtool-python/sgtool_py/core/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Core geophysical processing algorithms adapted from SGTool."""
-
-from .geophysical_processor import GeophysicalProcessor
-from .frequency_filters import FrequencyFilters
-from .gradient_filters import GradientFilters
-
-__all__ = ["GeophysicalProcessor", "FrequencyFilters", "GradientFilters"]
